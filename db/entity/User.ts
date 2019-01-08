ad9c4458565164f07127231e3b664bf9c87cd421
andrewyan-taroko <andrew.yan@tarokosoftware.com>
2019-01-08 16:48:20 +0800

    setup database

---
+++ db/entity/User.ts
@@ -0,0 +1,14 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column()
+  public email!: string;
+}
