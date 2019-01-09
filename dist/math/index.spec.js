bd0f49d61042eb517dff3536bbf81de778be68dd
Andrew Yan <zushenyan@gmail.com>
2019-01-09 22:16:51 +0800

    remove ignored files

--- dist/math/index.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-describe('add', () => {
-    it('should work', () => {
-        expect(index_1.add(1, 2)).toEqual(3);
-    });
-});
-//# sourceMappingURL=index.spec.js.map
